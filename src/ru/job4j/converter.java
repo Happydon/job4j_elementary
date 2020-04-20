a1473033ec7f26be73ac9a235aea9ae472c63bb4
Happydon <ilijadavincy@gmail.com>
2020-04-20 23:35:57 +0300

    0. Что такое тестирование.[#263914]

--- src/ru/job4j/converter.java
+++ src/ru/job4j/converter.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-public class converter {
+public class Converter {
     public static int rubleToEuro(int value) {
         int rsl = value / 80;
         return rsl;
@@ -15,25 +15,36 @@ public class converter {
         return rsl;
     }
 
-    public static int DollarToRuble(int value) {
+    public static int dollarToRuble(int value) {
         int rsl = value * 70;
         return rsl;
     }
-    public static int EuroToRuble(int value) {
+    public static int euroToRuble(int value) {
         int rsl = value * 80;
         return rsl;
     }
 
-    public static int FuntToRuble(int value) {
+    public static int funtToRuble(int value) {
         int rsl = value * 70;
         return rsl;
     }
     public static void main(String[] args) {
-        int Euro = converter.rubleToEuro(160);
-        int Dollar = converter.rubleToDollar(280);
-        int Funt = converter.rubleToFunt(900);
-        System.out.println("160 rubles are " + Euro + " euro.");
-        System.out.println("280 rubles are " + Dollar + " dollar.");
-        System.out.println("900 rubles are " + Funt + " funt.");
+        int in = 160;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("160 rubles are 2. Test result : " + passed);
+        int euro = Converter.rubleToEuro(160);
+        System.out.println("160 rubles are " + euro + " euro.");
+        in = 280;
+        expected = 4;
+        System.out.println("280 rubles are 4. Test result : " + passed);
+        int dollar = Converter.rubleToDollar(280);
+        System.out.println("280 rubles are " + dollar + " dollar.");
+        in = 900;
+        expected = 10;
+        System.out.println("900 rubles are 10. Test result : " + passed);
+        int funt = Converter.rubleToFunt(900);
+        System.out.println("900 rubles are " + funt + " funt.");
     }
 }
