73f338a30dc38cbad47b793d5d812acf3187bb45
Happydon <ilijadavincy@gmail.com>
2020-04-30 22:26:30 +0300

    6.0.2. Заполнение массива.[#263848]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,9 @@ public class ArrayDefinition {
         names[1] = "Василий";
         names[2] = "Кирилл";
         names[3] = "Мефодий";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
