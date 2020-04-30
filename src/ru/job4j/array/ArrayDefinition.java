44929c51c0afa0b9cd1409ca6dcb3470532c7472
Happydon <ilijadavincy@gmail.com>
2020-04-30 22:25:13 +0300

    6.0.2. Заполнение массива.[#263848]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,10 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Григорий";
+        names[1] = "Василий";
+        names[2] = "Кирилл";
+        names[3] = "Мефодий";
     }
 }
