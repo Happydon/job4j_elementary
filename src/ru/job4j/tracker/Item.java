906273b08a9c1cdaa72667a44295453d6708a7f1
Happydon <ilijadavincy@gmail.com>
2020-07-22 22:52:20 +0300

    2. Перегрузка конструктора. [#264002]

---
+++ src/ru/job4j/tracker/Item.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+public class Item {
+    private String id;
+    private String name;
+    public Item() {
+    }
+    public Item(String id) {
+    }
+    public Item(String name, String id) {
+    }
+    public static void main(String[] args) {
+        Item item = new Item();
+    }
+}
