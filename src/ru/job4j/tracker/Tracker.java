3b6c2edbe3b12adb832cd11dfc48a280fad2b8e5
Happydon <ilijadavincy@gmail.com>
2020-07-28 20:53:40 +0300

    6. Метод замены заявки. Tracker.replace [#264012]

--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -25,14 +25,27 @@ public class Tracker {
         }
         return Arrays.copyOf(itemsWithThisName, size);
     }
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
+        }
+        return rsl;
+    }
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+    public boolean replace(int id, Item item) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = item;
+            items[index].setId(id);
+            rsl = true;
         }
         return rsl;
     }
