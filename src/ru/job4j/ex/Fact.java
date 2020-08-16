09d7f27f3350c1b98e862fd908723c0db5c5ec11
Happydon <ilijadavincy@gmail.com>
2020-08-17 00:36:29 +0300

    0.3. Тестирование исключений с junit [#263976]

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -1,21 +1,12 @@
 package ru.job4j.ex;
 
-import java.util.Scanner;
-
 public class Fact {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input the number ");
-        int n = Integer.parseInt(input.nextLine());
-        System.out.println("Factorial of " + n + " is " + calc(n));
-    }
-
     public static int calc(int n) {
         if (n < 0) {
-        throw new IllegalArgumentException("Number should be equal or bigger than zero");
-    }
+            throw new IllegalArgumentException("N could not be less then 0");
+        }
         int rsl = 1;
-        for (int index = 1; index <= n; index++) {
+        for (int index = 1; index != n; index++) {
             rsl *= index;
         }
         return rsl;
